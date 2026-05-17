a9295d22cb600b80c11a2e8953e62a52cdfbad66
raccoonlinlin <raccoonlinlin20010119@gmail.com>
2026-05-17 22:06:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
+import { fileURLToPath } from 'url'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-  base: '/raccoonlinlin/',
+export default defineConfig({
+ 
+  base: '/',
 
   plugins: [
     react(),
