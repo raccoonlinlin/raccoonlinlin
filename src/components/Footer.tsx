01f148865138567f08485b6b8bef27f42638caec
raccoonlinlin <raccoonlinlin20010119@gmail.com>
2026-05-24 13:26:15 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer: React.FC = () => {
   return (
     <footer className="py-16 text-center text-gray-400 text-sm border-t border-white/20 glass-effect mt-auto">
-      <p>&copy; 2022-2026 RACCOONLINLIN. All Rights Reserved.</p>
+      <p>&copy; 2022 RACCOONLINLIN. All Rights Reserved.</p>
     </footer>
   );
 };
