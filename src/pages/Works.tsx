872d2270ba00fb9f439505ea3b21980fe12a170f
raccoonlinlin <raccoonlinlin20010119@gmail.com>
2026-05-24 20:51:55 +0800

    Update Works.tsx

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -43,13 +43,5 @@ export const Works: React.FC = () => {
       <div className="max-w-4xl mx-auto px-6">
         <div className="h-px bg-pink-100 w-full" />
       </div>
-
-      <ProductGrid 
-        id="shopee"
-        data={SHOPEE_DATA} 
-        title="蝦皮賣場熱銷商品" 
-        subtitle="Shopee Bestsellers & Stationery" 
-      />
-    </div>
   );
 };
